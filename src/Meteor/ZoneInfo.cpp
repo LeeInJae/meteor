33c77e68181e573b9f2e31085e60d17512f46ce3
SlowT <slowt@naver.com>
2013-11-05 21:22:21 -0800

    빈함수

--- src/Meteor/ZoneInfo.cpp
+++ src/Meteor/ZoneInfo.cpp
@@ -10,3 +10,23 @@ CZoneInfo::CZoneInfo(void)
 CZoneInfo::~CZoneInfo(void)
 {
 }
+
+bool CZoneInfo::LoadResource()
+{
+	return false;
+}
+
+void CZoneInfo::Release()
+{
+
+}
+
+bool CZoneInfo::LoadMap()
+{
+	return false;
+}
+
+bool CZoneInfo::LoadObjects()
+{
+	return false;
+}
